9865c6f8af224d8b2c44ab2461c2ec6a13a36cff
TuMercado <tumercado17@gmail.com>
2017-05-13 11:21:22 -0300

    sadasdasd

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+    define('SERVIDOR_CORREO',"smtp.gmail.com");
+    define('REMITENTE', 'Administrador <gallardoignacio1995@gmail.com>');
+    define('DIRECCION_RESPUESTA', 'gallardoignacio1995@gmail.com');
+    define('DESTINATARIO_APLICACIONES','gallardoignacio1995@gmail.com');
+    define('CON_COPIA','gallardoignacio1995@gmail.com');
+    define('COPIA_OCULTA','gallardoignacio1995@gmail.com');
+    
+    
+?>
